1625b1b742989382d7f1fde49210c0e1c61e6e68
Deep Shah <48095548+DeepShah99@users.noreply.github.com>
2022-06-28 18:23:33 -0400

    Update listUsers.py

--- IAM/listUsers.py
+++ IAM/listUsers.py
@@ -1,9 +1,11 @@
 import boto3
 iam = boto3.client("iam")
 
-def list_users():
-    response = iam.list_users()
-    print(response)
-
+def list_users(): 
+    #response = iam.list_users() # method 1
+    #print(response)
+    for user in iam.list_users()["Users"]:  #improved method 2 
+       print(user["UserName"])
+    
 
 list_users()
